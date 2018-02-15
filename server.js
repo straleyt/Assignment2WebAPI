12c77d059ec791a09c9435e525670d80165d4dd9
straleyt <tegan.straley@ucdenver.edu>
2018-02-15 10:22:18 -0700

    'update'

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var authController = require('./auth');
-var authJwtController = require('./auth_jwt');
-db = require('./db')(); //global hack
+
 var jwt = require('jsonwebtoken');
 var port = 8080;
 
@@ -34,54 +32,6 @@ router.route('/posts')
         }
     );
 
-//When using http://localhost:8080/postjwt
-router.route('/postjwt')
-    .post(authJwtController.isAuthenticated, function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
-            }
-            res.send(req.body);
-        }
-    );
-
-//When using http://localhost:8080/gets
-router.post('/gets', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please pass username and password.'});
-    } else {
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password
-        };
-        // save the user
-        db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successful created new user.'});
-    }
-});
-
-//When using http://localhost:8080/deletes
-router.post('/deletes', function(req, res) {
-
-        var user = db.findOne(req.body.username);
-
-        if (!user) {
-            res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
-        }
-        else {
-            // check if password matches
-            if (req.body.password == user.password)  {
-                var userToken = { id : user.id, username: user.username };
-                var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json({success: true, token: 'JWT ' + token});
-            }
-            else {
-                res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
-            }
-        };
-});
 
 //Important. On port 8080
 app.use('/', router);
