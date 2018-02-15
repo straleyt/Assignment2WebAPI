f1abab6a934f03a88135ef3dc46ac232107a2363
straleyt <tegan.straley@ucdenver.edu>
2018-02-15 12:33:23 -0700

    'swagger'

--- server.js
+++ server.js
@@ -4,9 +4,9 @@
 //Packages should be a dependency in the package.json 
 var express = require('express');
 var http = require('http');
+var url = requiree('url');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-
 var jwt = require('jsonwebtoken');
 var port = 8080;
 
@@ -14,23 +14,22 @@ var port = 8080;
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(passport.initialize());
 
 var router = express.Router();
 
-//When using http://localhost:8080/posts
-router.route('/posts')
-    .post( function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
-            }
-            res.send(req.body);
-        }
-    );
+app.post('/posts', function(req,res){
+    //If no headers or body say so
+    if(req.headers.empty){
+        res.send("no headers sent");
+    } else{
+        res.send("Headers sent, lets read them out");
+    }
+
+
+});
+
+
 
 
 //Important. On port 8080
